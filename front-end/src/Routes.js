294708dfdd727068e8beb1a82a236effb7731f10
ivanzhang <ivanzhang220820@gmail>
2023-01-15 17:55:18 -0500

    Add Routes

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -2,17 +2,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserInfoPage } from './pages/UserInfoPage';
 import {LoginPage} from "./pages/LoginPage";
 import {SignUpPage} from "./pages/SignUpPage";
+import {PrivateRoute} from "./auth/PrivateRoute";
 
 export const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact>
+                <PrivateRoute path="/" exact>
                     <UserInfoPage />
-                </Route>
-                <Route path="/login">
+                </PrivateRoute>
+                <PrivateRoute path="/login">
                     <LoginPage></LoginPage>
-                </Route>
+                </PrivateRoute>
                 <Route path="/signup">
                     <SignUpPage></SignUpPage>
                 </Route>
