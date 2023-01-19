71c08a77fc62f7b3afc95f153c5bffa9e992c1aa
ivanzhang <ivanzhang220820@gmail>
2023-01-18 22:39:46 -0500

    add jwt to api

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,9 @@
+import {signUpRoute} from "./signUpRoute";
 import { testRoute } from './testRoute';
+import {loginRoute} from "./loginRoute";
 
 export const routes = [
+    signUpRoute,
     testRoute,
+    loginRoute,
 ];
