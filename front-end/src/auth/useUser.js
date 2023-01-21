324b4dff14cc1aa8b3c19f54b63a897fe25aa2e0
ivanzhang <ivanzhang220820@gmail>
2023-01-20 22:38:20 -0500

    Add useUser and useToken.js

---
+++ front-end/src/auth/useUser.js
@@ -0,0 +1,25 @@
+import {useToken} from "./useToken";
+import {useEffect, useState} from "react";
+
+export const useUser = () => {
+    const[token] = useToken();
+
+    const getPayloadFromToken = token => {
+        const encodedPayload = token.split('.')[1];
+        return JSON.parse(atob(encodedPayload))
+    }
+
+    const [user, setUser] = useState(() => {
+        if(!token) return null;
+        return getPayloadFromToken(token);
+    });
+
+    useEffect(() => {
+       if (!token) {
+           setUser(null);
+       } else {
+           setUser(getPayloadFromToken(token));
+       }
+    }, [token]);
+
+}
