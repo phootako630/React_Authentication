292ba9e71bf2b899438a7d138cc54d164eebe00f
ivanzhang <ivanzhang220820@gmail>
2023-03-16 12:40:00 -0400

    Add Mongo Altas url

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -3,7 +3,8 @@ import { MongoClient } from 'mongodb';
 let client;
 
 export const initializeDbConnection = async () => {
-    client = await MongoClient.connect('mongodb://localhost:27017', {
+    const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+    client = await MongoClient.connect(connectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
