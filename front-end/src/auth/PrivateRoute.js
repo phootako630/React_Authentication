dc1a4b9a637bb92da318ebd1eca59416f902ac5c
ivanzhang <ivanzhang220820@gmail>
2023-03-16 21:33:21 -0400

    Check production env

--- front-end/src/auth/PrivateRoute.js
+++ front-end/src/auth/PrivateRoute.js
@@ -2,7 +2,7 @@ import {Redirect, Route} from 'react-router-dom';
 import {useUser} from './useUser';
 
 export const PrivateRoute = props => {
-    const user = useUser();
+    const [user] = useUser();
     console.log('User:', user)
 
     if (!user || user.id === null) return <Redirect to="/login" />;
